7f5445670646224f79c5f61c3bee2118a1039a19
gonghelen7 <gonghelen12@gmail.com>
2022-10-05 12:53:06 -0400

    HE GONG 1005867803 QUIZ 4

--- src/main/java/DomesticatableTrader.java
+++ src/main/java/DomesticatableTrader.java
@@ -31,7 +31,6 @@ class DomesticatableTrader extends Trader<Domesticatable> {
         super(money);
     }
 
-
     @Override
     public int getSellingPrice(Domesticatable item) {
         int super_price = super.getSellingPrice(item);
